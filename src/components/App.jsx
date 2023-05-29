a1fce1fc6e2deaf3bb8ac86ca02616493f9e9e93
Elena Berezina <farfalina1908@gmail.com>
2023-05-29 17:46:12 +0300

    made header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Products from '../pages/Products/Products';
+import NotFound from '../pages/NotFound/NotFound';
+import { Container, Header, Logo, Link } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/"> Home</Link>
+          <Link to="/about"> About</Link>
+          <Link to="/products"> Products</Link>
+        </nav>
+      </Header>
+
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes> */}
+    </Container>
   );
 };
