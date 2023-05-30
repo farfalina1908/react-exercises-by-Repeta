ec64b94f85537f309731a682b007a4b3982ee106
Elena Berezina <farfalina1908@gmail.com>
2023-05-30 17:52:36 +0300

    update components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
-import Products from '../pages/Products/Products';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
+import { Products } from '../pages/Products/Products';
+import { ProductDetails } from '../pages/ProductDetails/ProductDetails';
 import NotFound from '../pages/NotFound/NotFound';
-import { Container, Header, Logo, Link } from './App.styled';
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{' '}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/"> Home</Link>
-          <Link to="/about"> About</Link>
-          <Link to="/products"> Products</Link>
-        </nav>
-      </Header>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
 
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
         <Route path="*" element={<NotFound />} />
-      </Routes> */}
-    </Container>
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import { Container, CardWrapper, ProductName } from './ProductList.styles';
+
+export const ProductList = ({ products }) => {
+  return (
+    <Container>
+      {products.map(product => (
+        <CardWrapper key={product.id}>
+          <Link to={`${product.id}`}>
+            <img src="https://via.placeholder.com/200x100" alt="" />
+            <ProductName>{product.name}</ProductName>
+          </Link>
+        </CardWrapper>
+      ))}
+    </Container>
+  );
+};
